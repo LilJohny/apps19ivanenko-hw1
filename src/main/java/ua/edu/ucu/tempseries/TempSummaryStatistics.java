c744c673943e48e3564045f7fd26be8c1d267ed3
denis <ivanenko@ucu.edu.ua>
2019-10-14 23:34:42 +0300

    More Refactoring

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,10 +1,26 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    public final double maxTemp;
-    public final double minTemp;
-    public final double avgTemp;
-    public final double devTemp;
+    private final double maxTemp;
+    private final double minTemp;
+    private final double avgTemp;
+    private final double devTemp;
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public double getDevTemp() {
+        return devTemp;
+    }
     TempSummaryStatistics(double avg, double dev, double min, double max) {
         this.avgTemp = avg;
         this.devTemp = dev;
